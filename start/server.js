fd7d827fbb1ac659c28f0238acc9cd8fe7657caa
Katlego Moikabi <moikabik@invokesolutions.ac.za>
2024-05-26 11:45:25 +0200

    Added External Ports

--- start/server.js
+++ start/server.js
@@ -1,9 +1,9 @@
-const { Ignitor } = require('@adonisjs/ignitor')
+const { Ignitor } = require('@adonisjs/ignitor');
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
   .fireHttpServer((handler) => {
-    const Server = use('Server')
-    Server.getInstance().listen(process.env.PORT)
+    const Server = use('Server');
+    Server.getInstance().listen(process.env.PORT, '0.0.0.0');
   })
-  .catch(console.error)
+  .catch(console.error);
