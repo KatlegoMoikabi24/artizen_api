5d52bea9a06fa5866fc2d26a47d538488ab6506a
FowardPro <147938121+FowardPro@users.noreply.github.com>
2024-05-31 15:08:00 +0200

    com

--- database/seeds/UserSeeder.js
+++ database/seeds/UserSeeder.js
@@ -14,13 +14,13 @@ const User = use('App/Models/User');
 const Hash = use('Hash');
 
 class UserSeeder {
-  async run () {
+  async run() {
     await User.create({
-      name: 'Katlego',
-      surname: 'Moikabi',
-      username: 'katlego1424',
-      contacts: '0664301975',
-      email: 'katlego@gmail.com',
+      name: 'Foward',
+      surname: 'Nkuna',
+      username: 'Foward1424',
+      contacts: '0664301375',
+      email: 'Foward0@gmail.com',
       role: 'admin',
       password: await Hash.make('123456')
     });
